a3b98037a2f39f52873501daaee2795220e9d474
Adam dwi Julianto <81203366+adam23145@users.noreply.github.com>
2023-10-07 21:04:30 +0700

    update migration table

--- database/migrations/2023_10_03_132927_create_t_cases_table.php
+++ database/migrations/2023_10_03_132927_create_t_cases_table.php
@@ -15,12 +15,15 @@ class CreateTCasesTable extends Migration
     {
         Schema::create('t_cases', function (Blueprint $table) {
             $table->id();
-            $table->integer('date');
+            $table->date('date');
             $table->string('vector_type'); // Ubah jenis data sesuai kebutuhan, misalnya enum atau varchar
             $table->integer('cases_total');
             $table->char('regency_id')->nullable();
             $table->char('district_id')->nullable();
             $table->char('village_id')->nullable();
+            $table->foreign('regency_id')->references('id')->on('regencies');
+            $table->foreign('district_id')->references('id')->on('districts');
+            $table->foreign('village_id')->references('id')->on('villages');
             $table->foreignId('created_by')->nullable()->constrained('users');
             $table->foreignId('updated_by')->nullable()->constrained('users');
             $table->boolean('is_active')->default(true);
